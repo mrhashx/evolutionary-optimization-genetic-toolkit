59c99ad9407f964653864c670cccc2f41c459d0c
AH.Maniee <hs.bibak2014@gmail.com>
2021-03-27 11:30:00 +0330

    Establish final main orchestrator layout running seamless verification loops

---
+++ main_modular_orchestrator.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+from model import model
+from Individual import Individual
+import random
+import matplotlib.pyplot as plt
+from GA import GA
+def manage():
+    v = GA()
+    v.main()
+manage()
